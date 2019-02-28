4dbb8ac4ba0801c54d9b0fa4b84d972b24d6dfff
victorjaquez <victor@jaquez.me>
2019-02-28 10:02:18 -0700

    subscribed to the studentObservable to access student data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student.model';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-observables';
+export class AppComponent implements OnInit {
+  students: Student[] = [];
+
+  constructor(private studentservice: StudentService) {}
+
+  ngOnInit() {
+    // defining observable path in a variable
+    const studentObservable = this.studentservice.getStudents();
+    // subscribing to the observable, and filling students with the data
+    studentObservable.subscribe((studentsData: Student[]) => {
+      this.students = studentsData;
+    })
+
+  }
 }
