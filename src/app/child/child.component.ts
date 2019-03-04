e6b17941281a511ff6c19a256f4ad34700864b6a
victorjaquez <victor@jaquez.me>
2019-03-04 10:42:52 -0700

    onChange function in child, event emitter, @Output, event binding in parent, define ChildData in parent, interpolate child data in parent html

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,7 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
  
   @Input() ParentData: number;
+  @Output() childEvent =  new EventEmitter();
   constructor() { }
+
+  onChange(value) {
+    this.childEvent.emit(value);
+  }
 
   ngOnInit() {
   }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
+  public ChildData: number;
 
   constructor() { }
 
