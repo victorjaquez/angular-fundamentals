8fe409c5093665b890b7b4bc6c571ce43f10d1c2
victorjaquez <victor@jaquez.me>
2019-03-07 19:31:15 -0700

    created Singer model, and added data to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { Student } from './student.model';
-import { StudentService } from './student.service';
+// import { Student } from './student.model';
+// import { StudentService } from './student.service';
+import Singer from './Singer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  students: Student[] = [];
-
-  constructor(private studentservice: StudentService) {}
-
-  ngOnInit() {
-    // defining observable path in a variable
-    const studentObservable = this.studentservice.getStudents();
-    // subscribing to the observable, and filling students with the data
-    studentObservable.subscribe((studentsData: Student[]) => {
-      this.students = studentsData;
-    })
-
+export class AppComponent {
+  singers: Singer[] = [
+     {
+    'artist': 'Michael Jackson',
+    'country': 'USA'
+  },
+  {
+    'artist': 'Justin Bieber',
+    'country': 'Canada'
+  },
+  {
+    'artist': 'Daddy Yankee',
+    'country': 'Purto Rico'
+  },
+  {
+     'artist': 'A R Rehman',
+      'country': 'India'
+  },
+  {
+      'artist': 'Selena Gomez',
+      'country': 'USA'
   }
+ ];
 }
+
+
+
+
+
+
+// export class AppComponent implements OnInit {
+//   students: Student[] = [];
+
+//   constructor(private studentservice: StudentService) {}
+
+//   ngOnInit() {
+//     // defining observable path in a variable
+//     const studentObservable = this.studentservice.getStudents();
+//     // subscribing to the observable, and filling students with the data
+//     studentObservable.subscribe((studentsData: Student[]) => {
+//       this.students = studentsData;
+//     })
+
+//   }
+// }
